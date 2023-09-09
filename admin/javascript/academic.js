db104ac24b6a8c68ee6da9e1fe0022ff7a1a279b
ayb <ayyubiy67@gmail.com>
2023-09-09 21:23:37 +0100

    functionality for academic form

---
+++ admin/javascript/academic.js
@@ -0,0 +1,12 @@
+function check() 
+{
+        dom=document.academicfrm
+    var file=dom.userfile.value
+    
+        if(file!=["ce.pdf", "cp.pdf"])
+            {
+                alert("please chose correct file ");
+                return false;
+            }
+}
+// JavaScript Document
