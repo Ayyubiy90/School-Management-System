f3e5ffaa1ec9de36aa2591f55d328391ae3f4fa4
ayb <ayyubiy67@gmail.com>
2023-09-10 14:45:19 +0100

    added comment to del_all file js for to make it more clearer

--- admin/javascript/del_all.js
+++ admin/javascript/del_all.js
@@ -1,7 +1,12 @@
+// Function to handle deletion of all rows
 function del_all() {
+  // Access the 'deleteall' form in the document
   dom = deleteall.document;
 
-  if (this.confirm("Do you really want to Delete All Rows ?") == false)
-    return false;
-  else return true;
+  // Show a confirmation prompt asking if the user really wants to delete all rows
+  if (this.confirm("Do you really want to Delete All Rows ?") == false) {
+    return false; // If user selects 'Cancel', function will return false
+  } else {
+    return true; // If user selects 'OK', function will return true, allowing the deletion to proceed
+  }
 }
