2e5b3d680c3c54a2cc34e9833aa2035b29abdf09
ayb <ayyubiy67@gmail.com>
2023-09-10 14:42:57 +0100

    added comment to addstudent js file for more understanding

--- admin/javascript/addstudent.js
+++ admin/javascript/addstudent.js
@@ -1,37 +1,46 @@
+// Function to validate the 'addstudent' form fields
 function check() {
-    dom = document.addstudent;
+  // Store the form object in the 'dom' variable for easier access
+  dom = document.addstudent;
 
-    if (dom.id.value == "") {
-        alert("Please enter Student ID");
-        dom.id.focus();
-        return false;
-    }
-    if (dom.name.value == "") {
-        alert("Please enter Student Name");
-        dom.name.focus();
-        return false;
-    }
-    if (dom.standard.value == "") {
-        alert("Please enter Standard.");
-        dom.standard.focus();
-        return false;
-    }
+  // Check if the 'id' field is empty
+  if (dom.id.value == "") {
+    alert("Please enter Student ID");
+    dom.id.focus(); // Focus back on the 'id' input field
+    return false;
+  }
 
-    function IsNumeric(strString) {
-        // check for valid numeric strings
-        var strValidChars = "0123456789.-";
-        var strChar;
-        var bInResult = true;
+  // Check if the 'name' field is empty
+  if (dom.name.value == "") {
+    alert("Please enter Student Name");
+    dom.name.focus(); // Focus back on the 'name' input field
+    return false;
+  }
 
-        if (strString.length == 0) return false;
+  // Check if the 'standard' field is empty
+  if (dom.standard.value == "") {
+    alert("Please enter Standard.");
+    dom.standard.focus(); // Focus back on the 'standard' input field
+    return false;
+  }
 
-        // test strString consists of valid characters listed above
-        for (i = 0; i < strString.length && bInResult == true; i++) {
-            strChar = strString.charAt(i);
-            if (strValidChars.indexOf(strChar) == -1) {
-                bInResult = false;
-            }
-        }
-        return bInResult;
+  // Nested function to check if a given string is numeric
+  function IsNumeric(strString) {
+    // Define valid numeric characters
+    var strValidChars = "0123456789.-";
+    var strChar;
+    var bInResult = true;
+
+    // If the string is empty, return false
+    if (strString.length == 0) return false;
+
+    // Loop through each character to check if it's a valid numeric character
+    for (i = 0; i < strString.length && bInResult == true; i++) {
+      strChar = strString.charAt(i);
+      if (strValidChars.indexOf(strChar) == -1) {
+        bInResult = false;
+      }
     }
+    return bInResult; // Return the result (true if numeric, false otherwise)
+  }
 }
