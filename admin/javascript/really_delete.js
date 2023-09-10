5b1d2bbc233a9a031f30e66d1acd7deb276c9ffb
ayb <ayyubiy67@gmail.com>
2023-09-10 14:48:34 +0100

    added comment to really_delete

--- admin/javascript/really_delete.js
+++ admin/javascript/really_delete.js
@@ -1,7 +1,12 @@
-function really_delete() {
-  dom = document.deleteall;
+// Function to handle deletion of all rows
+function del_all() {
+  // Access the 'deleteall' form in the document
+  dom = deleteall.document;
 
-  prompt("Are you sure to Delete All ?");
-  if (ok) return true;
-  else return false;
+  // Show a confirmation prompt asking if the user really wants to delete all rows
+  if (this.confirm("Do you really want to Delete All Rows ?") == false) {
+    return false; // If user selects 'Cancel', function will return false
+  } else {
+    return true; // If user selects 'OK', function will return true, allowing the deletion to proceed
+  }
 }
