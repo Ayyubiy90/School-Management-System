aee27665dc9d875d69591b138bbedd2f5b7cc91f
ayb <ayyubiy67@gmail.com>
2023-09-10 13:57:42 +0100

    creating heading path...

---
+++ admin/add_faculty.php
@@ -0,0 +1,42 @@
+<?php require_once("adminOnly.php"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+
+<head>
+    <title>SCEM Admin panel</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="images/title.gif" rel="shortcut icon" type="image/x-icon">
+    <script type="text/JavaScript">
+        <!--
+function MM_jumpMenu(targ,selObj,restore){ //v3.0
+  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0;
+}
+//-->
+    </script>
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF">
+    <table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+            <td valign="top" height="18" colspan="2">
+                <style type="text/css">
+                    <!--
+                    .style1 {
+                        color: #336666
+                    }
+                    -->
+                </style>
+                <a name="top"></a>
+                <table cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td colspan="2"><img src="images/cellside.gif" width="1050" height="168"></td>
+                    </tr>
+
+                    <tr>
+                        <td height="2" colspan="2"></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
