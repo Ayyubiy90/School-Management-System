d2a082bc1b1c6bd83956cc849553af88aa9326b4
ayb <ayyubiy67@gmail.com>
2023-09-10 13:02:52 +0100

    admin notice

---
+++ admin/add_adm_notice.php
@@ -0,0 +1,15 @@
+<?php require_once("adminOnly.php");?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"> 
+<html> 
+<head> 
+<title>SCEM Admin panel</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+<link  rel="stylesheet" href="style.css" type="text/css">                                          <link href="images/title.gif" rel="shortcut icon" type="image/x-icon"></head> 
+ 
+<body  leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF"> 
+<table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0"> 
+  <tr> <td valign="top" height="18" colspan="2"> <style type="text/css"> 
+<!--
+.style1 {color: #336666}
+-->
+</style> 
