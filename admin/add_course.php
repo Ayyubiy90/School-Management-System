23ab9b4981beff25e588fae3a0ac064747194e8d
ayb <ayyubiy67@gmail.com>
2023-09-10 13:51:45 +0100

    table creation...

--- admin/add_course.php
+++ admin/add_course.php
@@ -91,3 +91,113 @@ function MM_jumpMenu(targ,selObj,restore){ //v3.0
                                 </tr>
                             </table>
                         </td>
+                        <td width="83%" valign="top">
+                            <table width="99%" border="0" cellspacing="1" cellpadding="1" align="center">
+                                <tr>
+                                    <td>
+                                        <table width="100%" border="0" cellspacing="1" cellpadding="1" align="center" class="border">
+                                            <tr>
+                                                <td height="225" bgcolor="#F4F4F4">
+                                                    <table cellpadding="0" cellspacing="0" width="100%">
+                                                        <tr>
+                                                            <td valign="middle" colspan="4" class=title>Add Standard </td>
+                                                        </tr>
+                                                    </table>
+                                                    <table width="96%" height="267" cellpadding="0" cellspacing="5" bgcolor="#F4F4F4">
+                                                        <tr>
+                                                        <tr>
+                                                            <td height="135">
+                                                                <p><a href="course.php"><img src="images/back.png" border="0" align="absmiddle"></a><br>
+                                                                    <?php
+                                                                    if ($_REQUEST['submit']) {
+
+
+                                                                        echo '<pre>';
+                                                                        $db = mysql_connect("localhost", "root", "");
+                                                                        mysql_select_db("student", $db);
+                                                                        $sql = "INSERT INTO standard 
+			VALUES('$_POST[id]','$_POST[standard_name]','$_POST[intake]','$_POST[fees]')";
+                                                                        $result = mysql_query($sql);
+                                                                        echo '<font face="verdana" color="#336666">';
+                                                                        echo '<u>';
+                                                                        echo $db;
+                                                                        echo '</u>';
+                                                                        echo "  added successfully....";
+                                                                        echo '</font>';
+                                                                    } else {
+                                                                        echo '<font face="verdana" color="#336666">';
+                                                                        echo "No file chosen..\n";
+                                                                        echo '</font>';
+                                                                    }
+
+                                                                    print "</pre>";
+
+                                                                    ?>
+                                                                    <script src="javascript/addcourse.js" language="javascript"></script>
+                                                                <form name="addcourseform" enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                                                                    <table width="90%" align="center" border="0" cellspacing="4" cellpadding="4">
+                                                                        <tr class="lgbg">
+                                                                            <td width="295" align="right" class="fheader">
+                                                                                <font style="font-size:12px"><b>Standard name</b></font>
+                                                                            </td>
+                                                                            <td class="formfontcaps">
+                                                                                <font face="Arial" size="+1">:</font>
+                                                                            </td>
+                                                                            <td><select name="standard_name" class="listmenu">
+                                                                                    <option>select</option>
+                                                                                    <option>FIRST</option>
+                                                                                    <option>SECOND</option>
+                                                                                    <option>THIRD</option>
+                                                                                    <option>FOURTH</option>
+                                                                                    <option>FIFTH</option>
+                                                                                    <option>SIXTH</option>
+                                                                                    <option>SEVENTH</option>
+                                                                                    <option>EIGHT</option>
+                                                                                    <option>NINTH</option>
+                                                                                    <option>TENTH</option>
+                                                                                    <option>ELEVAN</option>
+                                                                                    <option>TWELTH</option>
+                                                                                </select></td>
+                                                                        </tr>
+                                                                        <tr class="lgbg">
+                                                                            <td width="295" align="right" class="fheader">
+                                                                                <font style="font-size:12px"><b>Intake</b></font>
+                                                                            </td>
+                                                                            <td class="formfontcaps">
+                                                                                <font face="Arial" size="+1">:</font>
+                                                                            </td>
+                                                                            <td><input type="text" name="intake" size="20" /></td>
+                                                                        </tr>
+                                                                        <tr class="lgbg">
+                                                                            <td width="295" align="right">
+                                                                                <font style="font-size:12px"><b>Fees(/Annum)</b></font>
+                                                                            </td>
+                                                                            <td width="6" class="formfontcaps">
+                                                                                <font face="Arial" size="+1">:</font>
+                                                                            </td>
+                                                                            <td width="403"><input type="text" name="fees" size="20" /></td>
+                                                                        </tr>
+                                                                    </table>
+                                                                    <table width="59%" align="left" border="0" cellspacing="4" cellpadding="4">
+                                                                        <tr>
+                                                                            <td width="64%">&nbsp;</td>
+                                                                            <td width="36%"></td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td></td>
+                                                                            <td align="center"><input type="submit" name="submit" value="Submit" onClick="return check()" /></td>
+                                                                        </tr>
+                                                                    </table>
+                                                                </form>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+
+                            </table>
+                        </td>
+                    </tr>
