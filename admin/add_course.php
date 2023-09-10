d20310140c18d0e634b90cafde37bb024c668721
ayb <ayyubiy67@gmail.com>
2023-09-10 13:50:34 +0100

    created table for add course

--- admin/add_course.php
+++ admin/add_course.php
@@ -16,40 +16,78 @@ function MM_jumpMenu(targ,selObj,restore){ //v3.0
 //-->
     </script>
 </head>
-<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF">
-  <table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-    <tr>
-      <td valign="top" height="18" colspan="2">
-        <style type="text/css">
-          <!--
-          .style1 {
-            color: #336666
-          }
-          -->
-        </style>
-        <a name="top"></a>
-        <table cellpadding="0" cellspacing="0">
-          <tr>
-            <td colspan="2"><img src="images/cellside.gif" width="1050" height="168"></td>
-          </tr>
 
-          <tr>
-            <td height="2" colspan="2"></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td valign="top" colspan="2">
-        <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%">
-          <tr>
-            <td width="13%" background="images/side.gif" valign="top">
-              <style type="text/css">
-                <!--
-                .style1 {
-                  font-size: 13px;
-                  font-weight: bold;
-                  color: #99FF00;
-                }
-                -->
-              </style>
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF">
+    <table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+            <td valign="top" height="18" colspan="2">
+                <style type="text/css">
+                    <!--
+                    .style1 {
+                        color: #336666
+                    }
+                    -->
+                </style>
+                <a name="top"></a>
+                <table cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td colspan="2"><img src="images/cellside.gif" width="1050" height="168"></td>
+                    </tr>
+
+                    <tr>
+                        <td height="2" colspan="2"></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td valign="top" colspan="2">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%">
+                    <tr>
+                        <td width="13%" background="images/side.gif" valign="top">
+                            <style type="text/css">
+                                <!--
+                                .style1 {
+                                    font-size: 13px;
+                                    font-weight: bold;
+                                    color: #99FF00;
+                                }
+                                -->
+                            </style>
+                            <table width="160" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td height="26" background="but.gif">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="index.php">
+                                            <font color="#363636" style="font-size:12px">Home</font>
+                                        </a></td>
+                                </tr>
+
+
+                                <tr>
+                                    <td height="26" background="but.gif">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="course.php">
+                                            <font color="#363636">Standard</font>
+                                        </a></td>
+                                </tr>
+
+                                <tr>
+                                    <td height="26" background="but.gif">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="staff.php">
+                                            <font color="#363636">Staff</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26" background="but.gif">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="admission.php">
+                                            <font color="#363636" style="font-size:12px">Admission</font>
+                                        </a></td>
+                                </tr>
+
+
+                                <tr>
+                                    <td height="26" background="but.gif">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="adminLogOut.php">
+                                            <font color="#363636" style="font-size:12px">Logout</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+
+
+                                </tr>
+                            </table>
+                        </td>
