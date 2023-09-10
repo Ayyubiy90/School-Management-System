0d270b3911f504734f4900eed6b50f8e23931059
ayb <ayyubiy67@gmail.com>
2023-09-10 13:05:18 +0100

    adding style effect

--- admin/add_adm_notice.php
+++ admin/add_adm_notice.php
@@ -1,15 +1,48 @@
-<?php require_once("adminOnly.php");?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"> 
-<html> 
-<head> 
-<title>SCEM Admin panel</title> 
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
-<link  rel="stylesheet" href="style.css" type="text/css">                                          <link href="images/title.gif" rel="shortcut icon" type="image/x-icon"></head> 
- 
-<body  leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF"> 
-<table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0"> 
-  <tr> <td valign="top" height="18" colspan="2"> <style type="text/css"> 
-<!--
-.style1 {color: #336666}
--->
-</style> 
+<?php require_once("adminOnly.php"); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+
+<head>
+    <title>SCEM Admin panel</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="images/title.gif" rel="shortcut icon" type="image/x-icon">
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF">
+    <table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+            <td valign="top" height="18" colspan="2">
+                <style type="text/css">
+                    <!--
+                    .style1 {
+                        color: #336666
+                    }
+                    -->
+                </style>
+                <a name="top"></a>
+                <table cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td colspan="2"><img src="images/cellside.gif" width="1050" height="168"></td>
+                    </tr>
+
+                    <tr>
+                        <td height="2" colspan="2"></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td valign="top" colspan="2">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%">
+                    <tr>
+                        <td width="13%" background="images/side.gif" valign="top">
+                            <style type="text/css">
+                                <!--
+                                .style1 {
+                                    font-size: 13px;
+                                    font-weight: bold;
+                                    color: #99FF00;
+                                }
+                                -->
+                            </style>
