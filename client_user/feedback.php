0f60517207e665ce846e007d6fb5bacbda57b980
ayb <ayyubiy67@gmail.com>
2023-09-12 18:34:17 +0100

    Add basic structure with header and navigation menu for SAHJANAND VIDHAYALAY website

---
+++ client_user/feedback.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"> 
+<html> 
+<head> 
+<title>::SAHJANAND VIDHAYALAY,RAJKOT::</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+<link  rel="stylesheet" href="style.css" type="text/css"> 
+ <link href="images/title.gif" rel="shortcut icon" type="image/x-icon"> </head> 
+ 
+<body  leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#E2F0FE"> 
+<table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0"> 
+  <tr> 
+    <td valign="top" height="18" colspan="2"> 
+      <style type="text/css"> 
+<!--
+.style1 {color: #336666}
+-->
+</style> 
+<a  name="top"></a> 
+<table cellpadding="0" cellspacing="0"> 
+  <tr>
+    <td colspan="2"><img  src="images/banner.gif" width="1021" height="103"></td> 
+  </tr> 
+  <tr> 
+    <td height="2" colspan="2"></td> 
+  </tr> 
+  <tr> 
+    <td height="2" colspan="2"></td> 
+  </tr> 
+  <tr> <td height="21" colspan="2" align="right" background="images/toplinkbg.gif">  <p>&nbsp;&nbsp;&nbsp; <a  href="index.php"><img src="images/link_home.gif" align="middle" border="0"><img src="images/separator.gif" align="middle" border="0"></a><a  href="about.php"><img src="images/link_about.gif" align="middle" border="0"><img src="images/separator.gif" align="middle" border="0"></a><a  href="contact.php"><img src="images/link_contact.gif"  align="middle" border="0"></a> </p></td> </tr> 
+  <tr> 
+    <td height="2" colspan="2"></td> 
+  </tr> 
+</table> 
+</td> 
+  </tr> 
+  <tr> 
+    <td valign="top" colspan="2"> 
+      <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%"> 
+        <tr><td width="13%" background="images/side.gif"  valign="top"> <style type="text/css"> 
+<!--
+.style1 {
+	font-size: 13px;
+	font-weight: bold;
+	color: #99FF00;
+}
+-->
+</style> 
+<table width="160" border="0" cellspacing="0" cellpadding="0">  <tr> 
+    <tr> 
+    <td height="26"><a  href="index.php"><font size="+1">Home</font></a></td> 
+  </tr> 
+  <tr> 
+    <td height="26"><a  href="about.php"><font size="+1">About us</font></a></td> 
+  </tr> 
+  <tr> 
+    <td height="26" ><a  href="standard.php"><font size="+1">Standard</font></a></td> 
+  </tr> 
+  <tr> 
+    <td height="26" ><a  href="STAFF.php"><font size="+1">Staff</font></a></td> 
+  </tr> 
+  <tr> 
+    <td height="26" ><a  href="admission.php"><font size="+1">Admission</font></a></td> 
+  </tr> 
+ 
+  <tr> 
+    <td height="26" ><a  href="activity.php"><font size="+1">StudentActivity</font></a></td> 
+  </tr> 
+  <tr> 
+    <td height="26" ><a  href="contact.php"><font size="+1">Contact us</font></a></td> 
+  </tr> 
+  <tr> 
+    <td height="26" ><a  href="feedback.php"><font size="+1">Feedback</font></a></td> </tr>
+    <td>&nbsp;</td> 
+  </tr> 
+  </table> 
+</td>
