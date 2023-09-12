e9415f5af062b9ee02791580fba35fbf886967cb
ayb <ayyubiy67@gmail.com>
2023-09-12 20:26:30 +0100

    Refactor the code and add Home field, About us field, Standard field, Staff field, Admission field, Student activity field, Contact us field and Feedback field

--- client_user/standard.php
+++ client_user/standard.php
@@ -55,3 +55,49 @@
                                 }
                                 -->
                             </style>
+                            <table width="160" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td height="26"><a href="index.php">
+                                            <font size="+1">Home</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="about.php">
+                                            <font size="+1">About us</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="standard.php">
+                                            <font size="+1">Standard</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="STAFF.php">
+                                            <font size="+1">Staff</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="admission.php">
+                                            <font size="+1">Admission</font>
+                                        </a></td>
+                                </tr>
+
+                                <tr>
+                                    <td height="26"><a href="activity.php">
+                                            <font size="+1">StudentActivity</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="contact.php">
+                                            <font size="+1">Contact us</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="feedback.php">
+                                            <font size="+1">Feedback</font>
+                                        </a></td>
+                                </tr>
+                                <td>&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
