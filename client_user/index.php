b6f2659d0b9115810f18dedc7fc7aae02b3cd2dd
ayb <ayyubiy67@gmail.com>
2023-09-12 18:45:06 +0100

    ..

---
+++ client_user/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+
+<head>
+    <title>:SAHJANAND VIDHAYALAY,RAJKOT::</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="images/title.gif" rel="shortcut icon" type="image/x-icon">
+
+    <style type="text/css">
+        <!--
+        .style2 {
+            font-size: 18px;
+            color: #999999;
+        }
+
+        .style5 {
+            font-size: 1px
+        }
+        -->
+    </style>
+</head>
