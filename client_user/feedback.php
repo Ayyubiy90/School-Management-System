fe49e611e238b977d3147e98684fd826d9733e3a
ayb <ayyubiy67@gmail.com>
2023-09-12 18:36:08 +0100

    Add Feedback section with form for collecting user details

--- client_user/feedback.php
+++ client_user/feedback.php
@@ -1,76 +1,146 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"> 
-<html> 
-<head> 
-<title>::SAHJANAND VIDHAYALAY,RAJKOT::</title> 
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
-<link  rel="stylesheet" href="style.css" type="text/css"> 
- <link href="images/title.gif" rel="shortcut icon" type="image/x-icon"> </head> 
- 
-<body  leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#E2F0FE"> 
-<table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0"> 
-  <tr> 
-    <td valign="top" height="18" colspan="2"> 
-      <style type="text/css"> 
-<!--
-.style1 {color: #336666}
--->
-</style> 
-<a  name="top"></a> 
-<table cellpadding="0" cellspacing="0"> 
-  <tr>
-    <td colspan="2"><img  src="images/banner.gif" width="1021" height="103"></td> 
-  </tr> 
-  <tr> 
-    <td height="2" colspan="2"></td> 
-  </tr> 
-  <tr> 
-    <td height="2" colspan="2"></td> 
-  </tr> 
-  <tr> <td height="21" colspan="2" align="right" background="images/toplinkbg.gif">  <p>&nbsp;&nbsp;&nbsp; <a  href="index.php"><img src="images/link_home.gif" align="middle" border="0"><img src="images/separator.gif" align="middle" border="0"></a><a  href="about.php"><img src="images/link_about.gif" align="middle" border="0"><img src="images/separator.gif" align="middle" border="0"></a><a  href="contact.php"><img src="images/link_contact.gif"  align="middle" border="0"></a> </p></td> </tr> 
-  <tr> 
-    <td height="2" colspan="2"></td> 
-  </tr> 
-</table> 
-</td> 
-  </tr> 
-  <tr> 
-    <td valign="top" colspan="2"> 
-      <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%"> 
-        <tr><td width="13%" background="images/side.gif"  valign="top"> <style type="text/css"> 
-<!--
-.style1 {
-	font-size: 13px;
-	font-weight: bold;
-	color: #99FF00;
-}
--->
-</style> 
-<table width="160" border="0" cellspacing="0" cellpadding="0">  <tr> 
-    <tr> 
-    <td height="26"><a  href="index.php"><font size="+1">Home</font></a></td> 
-  </tr> 
-  <tr> 
-    <td height="26"><a  href="about.php"><font size="+1">About us</font></a></td> 
-  </tr> 
-  <tr> 
-    <td height="26" ><a  href="standard.php"><font size="+1">Standard</font></a></td> 
-  </tr> 
-  <tr> 
-    <td height="26" ><a  href="STAFF.php"><font size="+1">Staff</font></a></td> 
-  </tr> 
-  <tr> 
-    <td height="26" ><a  href="admission.php"><font size="+1">Admission</font></a></td> 
-  </tr> 
- 
-  <tr> 
-    <td height="26" ><a  href="activity.php"><font size="+1">StudentActivity</font></a></td> 
-  </tr> 
-  <tr> 
-    <td height="26" ><a  href="contact.php"><font size="+1">Contact us</font></a></td> 
-  </tr> 
-  <tr> 
-    <td height="26" ><a  href="feedback.php"><font size="+1">Feedback</font></a></td> </tr>
-    <td>&nbsp;</td> 
-  </tr> 
-  </table> 
-</td>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+
+<head>
+    <title>::SAHJANAND VIDHAYALAY,RAJKOT::</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="images/title.gif" rel="shortcut icon" type="image/x-icon">
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#E2F0FE">
+    <table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+            <td valign="top" height="18" colspan="2">
+                <style type="text/css">
+                    <!--
+                    .style1 {
+                        color: #336666
+                    }
+                    -->
+                </style>
+                <a name="top"></a>
+                <table cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td colspan="2"><img src="images/banner.gif" width="1021" height="103"></td>
+                    </tr>
+                    <tr>
+                        <td height="2" colspan="2"></td>
+                    </tr>
+                    <tr>
+                        <td height="2" colspan="2"></td>
+                    </tr>
+                    <tr>
+                        <td height="21" colspan="2" align="right" background="images/toplinkbg.gif">
+                            <p>&nbsp;&nbsp;&nbsp; <a href="index.php"><img src="images/link_home.gif" align="middle" border="0"><img src="images/separator.gif" align="middle" border="0"></a><a href="about.php"><img src="images/link_about.gif" align="middle" border="0"><img src="images/separator.gif" align="middle" border="0"></a><a href="contact.php"><img src="images/link_contact.gif" align="middle" border="0"></a> </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td height="2" colspan="2"></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td valign="top" colspan="2">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0" height="100%">
+                    <tr>
+                        <td width="13%" background="images/side.gif" valign="top">
+                            <style type="text/css">
+                                <!--
+                                .style1 {
+                                    font-size: 13px;
+                                    font-weight: bold;
+                                    color: #99FF00;
+                                }
+                                -->
+                            </style>
+                            <table width="160" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                <tr>
+                                    <td height="26"><a href="index.php">
+                                            <font size="+1">Home</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="about.php">
+                                            <font size="+1">About us</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="standard.php">
+                                            <font size="+1">Standard</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="STAFF.php">
+                                            <font size="+1">Staff</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="admission.php">
+                                            <font size="+1">Admission</font>
+                                        </a></td>
+                                </tr>
+
+                                <tr>
+                                    <td height="26"><a href="activity.php">
+                                            <font size="+1">StudentActivity</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="contact.php">
+                                            <font size="+1">Contact us</font>
+                                        </a></td>
+                                </tr>
+                                <tr>
+                                    <td height="26"><a href="feedback.php">
+                                            <font size="+1">Feedback</font>
+                                        </a></td>
+                                </tr>
+                                <td>&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+            <td width="83%" valign="top" bgcolor="#E6FBF4">
+        <table width="99%" border="0" cellspacing="1" cellpadding="1" align="center">
+          <tr>
+            <td>
+              <table width="100%" border="0" cellspacing="1" cellpadding="1" align="center" class="border">
+                <tr>
+                  <td height="436" bgcolor="#E2F0FE">
+                    <table cellpadding="0" cellspacing="0" width="100%">
+                      <tr>
+                        <td valign="middle" colspan="4" background="images/header.gif" class=title><img src="images/decoration_head.png" align="absmiddle">&nbsp;&nbsp;Feedback&nbsp;&nbsp;<img src="images/decoration_head.png" align="absmiddle"></td>
+                      </tr>
+                    </table>
+                    <table width="100%" cellpadding="0" cellspacing="5" bgcolor="#E2F0FE">
+                      <tr>
+
+                      <tr>
+                        <td height="135">
+                          <p align="center">
+                            <font color="#006666" face="Verdana" style="font-size:12px"> <?php include 'response.php'; ?></font>
+                          </p>
+
+                          <script src="javascript/feedback.js" language="javascript"></script>
+                          <FORM name="feedbackfrm" METHOD="post" ACTION="<?php echo $_SERVER['PHP_SELF']; ?>">
+                            <TABLE ID="table10" align="center" BORDER="0" CELLPADDING="1" CELLSPACING="1" WIDTH="300" class="jeetesh" bgcolor="#e2fofe">
+                              <!-- MSTableType="layout" -->
+
+
+                              <TR class="lgbg">
+                                <TD CLASS="fheader" ALIGN="right">First Name:</TD>
+                                <TD CLASS="fheader" HEIGHT="29">
+                                  <INPUT NAME="fname" SIZE="22" CLASS="finput">
+                                </TD>
+                              </TR>
+                              <TR class="gbg">
+                                <TD CLASS="fheader" ALIGN="right">Last Name:</TD>
+                                <TD CLASS="fheader" HEIGHT="29">
+                                  <INPUT NAME="lname" SIZE="22" CLASS="finput">
+                                </TD>
+                              </TR>
+                              <TR class="lgbg">
+                                <TD CLASS="fheader" ALIGN="right">Gender:</TD>
+                                <TD CLASS="fheader" HEIGHT="29">
